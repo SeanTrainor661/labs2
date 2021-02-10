b9d0b2be0802cfbcf6a102d7157f168cde2f4d18
Sean Trainor <S.TRAINOR-2020@hull.ac.uk>
2021-02-10 10:06:35 +0000

    Add project files.

---
+++ labs2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace labs2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ResizableIntArray numbers = new ResizableIntArray();
+            bool repeat = true;
+            do
+            {
+                Console.WriteLine("Please enter an integer: ");
+                string line = Console.ReadLine();
+                if (line == "exit")
+                {
+                    repeat = false;
+                }
+                else
+                {
+                    try
+                    {
+                        int number = int.Parse(line);
+                        Console.WriteLine("The integer that you entered was: " + number);
+                        numbers.Add(number);
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("That was not an integer!");
+                    }
+                }
+            }
+            while (repeat);
+            numbers.WriteContentsToConsole();
+            Console.WriteLine("Average: " + numbers.Average());
+            Console.WriteLine("Max: " + numbers.Max());
+            Console.WriteLine("Min: " + numbers.Min());
+            numbers.Ascending();
+            numbers.Descending();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ labs2/ResizableIntArray.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace labs2
+{
+    class ResizableIntArray
+    {
+        int[] mStorage = new int[10];
+        int mCurrentIndex = 0;
+        public void Add(int pNumber)
+        {
+            mStorage[mCurrentIndex] = pNumber;
+            mCurrentIndex++;
+            if (IsFull())
+            {
+                ExpandStorage(10);
+            }
+        }
+        public void ExpandStorage(int pSizeIncrease)
+        {
+            int[] increasedStorage = new int[mCurrentIndex + pSizeIncrease];
+            for (int i = 0; i < mCurrentIndex; i++)
+            {
+                increasedStorage[i] = mStorage[i];
+            }
+            mStorage = increasedStorage;
+        }
+        public bool IsFull()
+        {
+            if (mCurrentIndex < mStorage.Length)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void WriteContentsToConsole()
+        {
+            for (int i = 0; i < mCurrentIndex; i++)
+            {
+                Console.WriteLine(mStorage[i]);
+            }
+        }
+        public string Average()
+        {
+            int Av = 0;
+            for (int i = 0; i < mCurrentIndex; i++)
+            {
+                Av += mStorage[i];
+            }
+            Av = Av / mCurrentIndex;
+            return Av.ToString();
+        }
+        public string Min()
+        {
+            int min = mStorage[0];
+            for (int i = 1; i < mCurrentIndex; i++)
+            {
+                if(mStorage[i] < min)
+                {
+                    min = mStorage[i];
+                }
+            }
+            return min.ToString();
+        }
+        public string Max()
+        {
+            int max = mStorage[0];
+            for (int i = 1; i < mCurrentIndex; i++)
+            {
+                if (mStorage[i] > max)
+                {
+                    max = mStorage[i];
+                }
+            }
+            return max.ToString();
+        }
+        public void Ascending() // insertion sort
+        {
+            for (int i = 1; i < mCurrentIndex; i++)
+            {
+                int val = mStorage[i];
+                int flag = 0;
+                for (int j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (val < mStorage[j])
+                    {
+                        mStorage[j + 1] = mStorage[j];
+                        j--;
+                        mStorage[j + 1] = val;
+                    }
+                    else flag = 1;
+                }
+            }
+            Console.WriteLine("Ascending numbers:");
+            for(int i = 0; i < mCurrentIndex; i++)
+            {
+                Console.WriteLine(mStorage[i]);
+            }
+        }
+        public void Descending() // insertion sort
+        {
+            for (int i = 1; i < mCurrentIndex; i++)
+            {
+                int val = mStorage[i];
+                int flag = 0;
+                for (int j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (val > mStorage[j])
+                    {
+                        mStorage[j + 1] = mStorage[j];
+                        j--;
+                        mStorage[j + 1] = val;
+                    }
+                    else flag = 1;
+                }
+            }
+            Console.WriteLine("Descending numbers:");
+            for (int i = 0; i < mCurrentIndex; i++)
+            {
+                Console.WriteLine(mStorage[i]);
+            }
+        }
+    }
+}
